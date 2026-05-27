ca2408a3988bcf535c0a41930909c6db03e26933
nvicharapu <61048623+nvicharapu@users.noreply.github.com>
2026-05-28 00:19:18 +0530

    Local Terminal executed tests in Remote Azure Cloud

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,13 +13,13 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
- /*
+ 
   use: {
   
     browserName: 'chromium',
       headless: false,
   },
-*/
+
   reporter: 'html',
 
   /* Configure projects for major browsers */
