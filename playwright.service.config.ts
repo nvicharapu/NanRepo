4c9706c965824f159fe73664b4a3e01a1e27f5de
nvicharapu <61048623+nvicharapu@users.noreply.github.com>
2026-05-28 13:20:59 +0530

    service file updated

--- playwright.service.config.ts
+++ playwright.service.config.ts
@@ -1,33 +1,20 @@
 import { defineConfig } from '@playwright/test';
 import { createAzurePlaywrightConfig, ServiceOS } from '@azure/playwright';
-import { DefaultAzureCredential } from '@azure/identity';
+import { AzureCliCredential } from '@azure/identity';
 import config from './playwright.config';
-import { getServiceConfig } from '@azure/playwright-testing';
 
-/* Learn more about service configuration at https://aka.ms/pww/docs/config */
 export default defineConfig(
-  {
-    ...getServiceConfig(),
-  },
   config,
   createAzurePlaywrightConfig(config, {
     exposeNetwork: '<loopback>',
-    connectTimeout: 3 * 60 * 1000, // 3 minutes
+    connectTimeout: 3 * 60 * 1000,
     os: ServiceOS.LINUX,
-    credential: new DefaultAzureCredential(),
+    credential: new AzureCliCredential(), // ✅ IMPORTANT CHANGE
   }),
   {
-    /*
-    Enable Playwright Workspaces Reporter:
-    Uncomment the reporter section below to upload test results and reports to Playwright Workspaces.
-
-    Note: The HTML reporter must be included before Playwright Workspaces Reporter.
-    This configuration will replace any existing reporter settings from your base config.
-    If you're already using other reporters, add them to this array.
-    */
     reporter: [
-      ["html", { open: "never" }],
-       ["@azure/playwright/reporter"],
-     ],
+      ['html', { open: 'never' }],
+      ['@azure/playwright/reporter'],
+    ],
   }
 );
