0cf1e8c8dce57235471517b5312bfaf83843739f
nvicharapu <61048623+nvicharapu@users.noreply.github.com>
2026-05-29 00:30:54 +0530

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,21 +13,69 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
- 
+  retries: 1,
+  timeout: 30000,
+  expect: {
+    timeout: 5000
+  },
+ /*
   use: {
   
     browserName: 'chromium',
       headless: false,
   },
-
+  */
   reporter: 'html',
+ // workers: 1,
+ // fullyParallel: true,
 
   /* Configure projects for major browsers */
   projects: [
+
+     /*
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] }
-    }
+     name: 'safari',
+      use: {
+        
+         ...devices['iPhone 13'],
+         headless: false,
+         trace : 'on',
+         screenshot: 'on',
+         video: 'on',
+         ignoreHTTPSErrors: true,
+         permissions: ['geolocation'],
+         geolocation: { longitude: 12.4924, latitude: 41.8902 },
+         locale: 'en-US',
+         //viewport: { width: 1280, height: 720 },
+         colorScheme: 'dark',
+         timezoneId: 'America/New_York',
+         userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Mobile/15E148 Safari/604.1'
+
+        }
+    },
+*/
+     {
+      name: 'chrome',
+      use: { 
+
+        ...devices['Desktop Chrome'] ,
+        headless: false,
+         trace : 'on',
+         screenshot: 'on',
+         video: 'on',
+         ignoreHTTPSErrors: true,
+         permissions: ['geolocation'],
+         geolocation: { longitude: 12.4924, latitude: 41.8902 },
+         locale: 'en-US',
+        // viewport: { width: 720, height: 720 },
+         colorScheme: 'dark',
+         timezoneId: 'America/New_York',
+         userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Mobile/15E148 Safari/604.1'
+
+        }
+
+      }
+
     
        ],
 
